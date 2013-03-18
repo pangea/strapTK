d6b80043394fa0fdcf66371e7102104de7b6bdfb
Chris Hall <chall@pangeaequity.com>
2013-03-18 17:11:23 -0500

    Wrote Fetcher

--- modules/utils.js
+++ modules/utils.js
@@ -77,7 +77,7 @@ function Typify(component, options) {
   if(!(component instanceof Component)) {
     throw TypeError("Typify can only operate on Components");
   }
-  
+
   options = _.extend({}, Typify.defaults, options);
 
   component.setType = function(type) {
@@ -116,7 +116,7 @@ Typify.defaults = {
 /**
  * Source adds data sources to Components.
  * This allows them to gather data from a remote API via AJAX or Websockets
- * 
+ *
  * Specifying a source URL with the websocket protocol (ws:// or wss://) will force
  *  the source to always be gathered via websocket.  If a websocket connection is not
  *  available, a WebsocketConnectError will be thrown.  Of note, this process will
@@ -130,27 +130,129 @@ Typify.defaults = {
  *
  * @throws TypeError if component is not an instance or subclass of Component
  */
- function Source(component, options) {
-   if(!(component instanceof Component)) {
+function Source(component, options) {
+  if(!(component instanceof Component)) {
     throw TypeError("Source can only operate on Components");
   }
-  
+
   options || (options = {});
-  
+
   component.sync = function() {
-    
+    //at the end of the sync
+    var event = $.Event("component.update");
+    event.target = this;
+    event.relatedTarget = this;
+    $(this).trigger(event);
   }
-  
+
   //set up data sourcing options
-  
-  component.fetcher = new Fetcher(component);
- }
- 
- /**
-  * Creates an XMLHTTPRequest or Websocket object
-  *
-  * @param component [Component] the parent object of this Fetcher
-  */
- function Fetcher(component) {
-   var p = this.parent = component;
- }
+
+  component.fetcher = new Fetcher();
+}
+
+/**
+ * Handles data transfer to and from (mostly from) an API
+ *   Can also connect one component to another component
+ *
+ * @param options [Object] the settings to apply to this Fetcher
+ *
+ * @throws SyntaxError
+ *  if options is not defined
+ *  if no source is given
+ *  if websocket is forced but no command is given
+ *  if the source is not valid (must be a String or Component)
+ *
+ * @throws WebsocketConnectError if websocket is forced but no connection is possible
+ */
+function Fetcher(options) {
+  if(!options) { throw new SyntaxError("You must define options for a Fetcher"); }
+  if(!options.source) { throw new SyntaxError("You must supply a source."); }
+  if(options.websocket && !options.command) { throw new SyntaxError("You must define a command when forcing WebSockets."); }
+
+  var websocket,
+      opened            = false,
+      id                = _.uniqueID("fetcher_"),
+      source            = options.source,
+      method            = options.method || "GET",
+      forceWebsocket    = options.websocket || false,
+      command           = options.command,
+      pollInterval      = options.pollInterval,
+      messageCallback   = options.callback || $.noop;
+
+  if(typeof(source) == "string") {
+    if(websockCommand) {
+      // auto executing function to create our websocket
+      // also creates a few closures, but that shouldn't be a problem
+      (function() {
+        opened = false;
+
+        websocket = new WebSocket(source);
+
+        // this is only used to determine if the websocket opened successfully
+        websocket.addEventListener("open", function() {
+          opened = true;
+        });
+
+        // used to properly perform fallbacks in the event of connection failure
+        websocket.addEventListener("close", function() {
+          if(!opened) {
+            if(source.match(/^ws/)) {
+              forceWebsocket = true;
+              source.replace(/^ws/, "http");
+              arguments.callee();
+            } else if(forceWebsocket) {
+              throw WebsocketConnectError("Unable to connect to "+source+" via WebSocket.");
+            } else {
+              websocket = false;
+            }
+          }
+        });
+
+        websocket.addEventListener("message", ,messageCallback);
+
+      })();
+    }
+
+  // Sourced Components are also valid data sources
+  } else if(source instanceof Component && source.data)  {
+    // subscribe to the source's update event
+    $(source).on("update."+id, messageCallback);
+
+  } else {
+    throw new SyntaxError("Invalid source type.");
+  }
+
+  /**
+   * Transfers data to or requests data from the source
+   */
+  this.sync = function(data) {
+    data || (data = command);
+    if(websocket) {
+      websocket.send(data);
+    } else if(typeof(source) == "string") {
+      $.ajax({
+        url: source,
+        data: data,
+        type: method,
+        success: messageCallback
+      });
+    } else {
+      messageCallback(null, source.data);
+    }
+  };
+
+  this.then = function(callback) {
+    if(!callback) { throw SyntaxError("You must supply a callback to Fetcher.then"); }
+    messageCallback = callback;
+
+    if(source instanceof Component) {
+
+      // reset update callback on sourced component
+      $(source).off("update."+id).on("update."+id, messageCallback);
+
+    } else if(websocket) {
+      websocket.removeEventListener("message");
+      websocket.addEventListener("message", messageCallback);
+    }
+  }
+}
