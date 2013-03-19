79536a12ef453b94eff1e13e4ac921231017403b
Chris Hall <chall@pangeaequity.com>
2013-03-19 10:13:03 -0500

    Not 100% sure what this is

---
+++ modules/errors.js
@@ -0,0 +1,3 @@
+var TooManyChildrenError  = Extend(Error, {message: "Too many children.", name: "TooManyChildrenError"}),
+
+    WebsocketConnectError = Extend(Error, {message: "Unable to connect via websocket.", name: "WebsocketConnectError"});
