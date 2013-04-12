de3413f3375d59eca2e379b1b9b86a996ba0f9ef
Chris Hall <chall@pangeaequity.com>
2013-04-12 14:48:04 -0500

    Added page-header class to PageHeaders

--- strapTK.js
+++ strapTK.js
@@ -1313,6 +1313,7 @@ var PageHeader = Header.extend({
       initialize: function(args) {
         PageHeader.__super__.initialize.call(this, args);
         this.setDefaultValue("", "header");
+        this.addClass("page-header");
       },
       template : _.template("<div <%= rootAttrs %>>"+
     													"<h<%= level %>>"+
