13c0b1978e2957f13f159d29c20f1891e4f75ffc
Chris Hall <chall@pangeaequity.com>
2013-05-03 16:56:01 -0500

    Deleted data key of attributes after parsing

--- modules/Panel.js
+++ modules/Panel.js
@@ -154,6 +154,7 @@ var Panel = Component.extend(
             _.each(attrs.data, function(val, key) {
               attrs["data-"+key] = val;
             });
+            delete attrs.data;
           }
 
           attrs = _.map(attrs, function(val, key) {
