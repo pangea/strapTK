2b895fac216f00e36df07cda3b109202ff8c1c00
Chris Hall <chall@pangeaequity.com>
2014-05-29 17:22:31 -0500

    Removed unnecessary files

--- modules/errors.js
+++
@@ -1,6 +0,0 @@
-/* Sprocket Manifest
- *= require Extend
- */
-var TooManyChildrenError  = Extend(Error, {message: "Too many children.", name: "TooManyChildrenError"}),
-
-    WebsocketConnectError = Extend(Error, {message: "Unable to connect via websocket.", name: "WebsocketConnectError"});
